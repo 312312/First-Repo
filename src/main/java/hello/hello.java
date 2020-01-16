a5eb17fc5d6b17a2a006951ef260ef62cc04bbcc
Rohit <rohitsaini@qainfotech.com>
2020-01-16 11:40:25 -0800

    Little changes

--- src/main/java/hello/hello.java
+++ src/main/java/hello/hello.java
@@ -2,9 +2,12 @@ package hello;
 
 public class hello {
 
-}
 public static void main(String[] args)
 {
 	
 	System.out.println("Hello");
+	System.out.println("Hello1");
+	System.out.println("Hello2");
+	System.out.println("Hello3");
+}
 }
--- src/test/java/hello123/h23.java
+++ src/test/java/hello123/h23.java
@@ -2,4 +2,9 @@ package hello123;
 
 public class h23 {
 
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Hello");
+	}
 }
